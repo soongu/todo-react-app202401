02a6298ad6224730728a8e9f8d468fb8bc6b598e
soongu <naong526@naver.com>
2024-01-11 09:50:50 +0900

    #7. TodoInput 입력내용 TodoTemplate으로 상향식 전달

--- src/component/TodoMain.js
+++ src/component/TodoMain.js
@@ -3,32 +3,7 @@ import React from 'react';
 import './scss/TodoMain.scss';
 import TodoItem from "./TodoItem";
 
-const TodoMain = () => {
-
-  const todoList = [
-    {
-      id: 1,
-      title: '장보기',
-      done: false
-    },
-    {
-      id: 2,
-      title: '저녁먹기',
-      done: true
-    },
-    {
-      id: 3,
-      title: '수다떨기',
-      done: false
-    },
-    {
-      id: 4,
-      title: '축구하기',
-      done: true
-    },
-
-  ];
-
+const TodoMain = ({todoList}) => {
 
   return (
     <ul className='todo-list'>
--- src/component/TodoTemplate.js
+++ src/component/TodoTemplate.js
@@ -6,11 +6,42 @@ import TodoMain from "./TodoMain";
 
 
 const TodoTemplate = () => {
+
+  /*
+      리액트는 부모컴포넌트에서 자식컴포넌트로의 데이터이동이 반대보다 쉽기 때문에
+      할 일 데이터는 상위부모컴포넌트에서 처리하는것이 좋다
+   */
+  const todoList = [
+    {
+      id: 1,
+      title: '장보기',
+      done: false
+    },
+    {
+      id: 2,
+      title: '저녁먹기',
+      done: true
+    },
+    {
+      id: 3,
+      title: '수다떨기',
+      done: false
+    },
+
+  ];
+
+  // 데이터 상향식 전달을 위해 부모가 자식에게 함수를 하나 전달
+  const addTodo = (todoText) => {
+    // console.log('할 일 등록 함수를 todotemplate에서 실행!');
+    console.log('todoText: ', todoText);
+  };
+
+
   return (
     <div className='TodoTemplate'>
       <TodoHeader />
-      <TodoMain />
-      <TodoInput />
+      <TodoMain todoList={todoList} />
+      <TodoInput onAdd={addTodo} />
     </div>
   );
 };
