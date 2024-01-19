94fb6abcee1ad6988fe5f219365206020c1e291f
soongu <naong526@naver.com>
2024-01-19 11:28:05 +0900

    #12. 리액트 라우터 돔으로 회원가입 페이지 전환

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoTemplate from "./component/TodoTemplate";
+import TodoTemplate from "./component/todo/TodoTemplate";
+import Header from "./component/layout/Header";
+import Footer from "./component/layout/Footer";
+import Join from "./component/user/Join";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <TodoTemplate />
+      <Header />
+
+      <Routes>
+        <Route path='/' element={ <TodoTemplate /> } />
+        <Route path='/join' element={ <Join /> }/>
+      </Routes>
+
+      <Footer />
     </>
   );
 }
